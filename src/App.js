a07386599c541d6748afc8d9c1bd522c5e7a2777
Lukas Blomdell <lukasblomdell@gmail.com>
2021-11-23 11:46:40 +0100

    Added Scrape on init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useEffect } from 'react';
 import './App.css';
 import GetRandomQuote from "./components/GetRandomQuote";
 import NavBar from "./components/NavBar"
 import ChatRoom from "./views/ChatRoom"
 
+const axios = require('axios');
+
+const client = axios.create({
+    baseURL: "http://localhost:8080/crema-spring-0.0.1-SNAPSHOT/api"
+});
+
 function App() {
+
+  useEffect(() => {
+    async function scrape() {
+        const response = await client.post("/quotes/scrape");
+        console.log(response.data);
+    }
+    scrape();
+}, [])
+  
   return (
     <div className="App">
       <NavBar></NavBar>

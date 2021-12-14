ca6a1dabfee79c93eab8a41d4272a061ee2248d4
Henrik Büller <henbul0928@skola.goteborg.se>
2021-12-14 11:41:24 +0100

    Refactor ChatbotEngine

--- src/components/ChatbotEngine.js
+++ src/components/ChatbotEngine.js
@@ -1,23 +1,56 @@
+const GREETING = "greeting";
+const QUESTION = "question";
+const STATEMENT = "statement";
 export default class ChatBotEngine {
     constructor() {
-        this.chatState = "greeting";
+        this.prevState = null;
+        this.currentState = GREETING;
+        this.nextState = QUESTION;
     }
 
     initialize() {
-        this.chatState = "question";
+        this.setStates();
         return "Hej! Vad har du för symptom?";
+    }
+
+    setStates() {
+        this.prevState = this.currentState;
+        this.currentState = this.nextState;
+        this.setNextState();
+    }
+
+    setNextState() {
+        switch (this.curentState) {
+            case GREETING:
+                this.nextState = QUESTION;
+                break;
+            case QUESTION:
+                this.nextState = STATEMENT;
+                break;
+            default:
+                this.nextState = STATEMENT;
+        }
     }
 
     getMessageCategory() {
         let category = "";
-        console.log("chatState before if: " + this.chatState);
-        if (this.chatState === "question") {
-            category = "question";
-            this.chatState = "statement";
-            console.log("change state to statement, check: " + this.chatState)
+
+        console.log("chatState before if: ");
+        this.logStates();
+
+        if (this.currentState === QUESTION) {
+            category = QUESTION;
+
+            this.setStates();
         } else {
-            category = "statement";
+            category = STATEMENT;
         }
         return category;
+    }
+
+    logStates() {
+        console.log("prev: " + this.prevState);
+        console.log("cur: " + this.currentState);
+        console.log("next: " + this.nextState);
     }
 }

de5872211e034f597e4889d72736a7c229f4e7b8
Henrik Büller <henbul0928@skola.goteborg.se>
2022-01-11 10:34:30 +0100

    Remove username in greeting

--- src/views/ChatRoom.js
+++ src/views/ChatRoom.js
@@ -4,12 +4,11 @@ import AuthService from "../services/auth.service";
 
 const ChatRoom = () => {
     const currentUser = AuthService.getCurrentUser();
-    console.log(currentUser.username);
 
     return (
         <main className="chat-room">
             <div className="chat-container">
-                <h1>Hej {currentUser.username}!</h1>
+                <h1>Hej insert username!</h1>
                 <ChatWindow></ChatWindow>
             </div>
         </main>
